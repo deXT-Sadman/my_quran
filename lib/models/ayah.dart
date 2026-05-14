9d8a2362960afd833ede7138d07f828e2152b576
Sadman <2258cse00945@manarat.ac.bd>
2026-05-14 13:38:58 +0600

    feat: Add Ayah model with properties for number, text, translation, and audio URL

---
+++ lib/models/ayah.dart
@@ -0,0 +1,13 @@
+class Ayah {
+  final int numberInSurah;
+  final String arabicText;
+  final String translation;
+  final String audioUrl;
+
+  Ayah({
+    required this.numberInSurah,
+    required this.arabicText,
+    required this.translation,
+    required this.audioUrl,
+  });
+}
